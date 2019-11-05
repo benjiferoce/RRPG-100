035edf0aa3eb8bbdf31788f176184d5d32f1a643
benjiferoce <31674458+benjiferoce@users.noreply.github.com>
2019-11-05 14:19:39 -0500

    Commit Initial Testing Environment

---
+++ RRPG 100/Assets/Scripts/movement.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// RRPG 100 Player Movement Script      // 11/05/2019 ------- Added basic movement and animation functionality //
+// Developed by Benjamin Weaver
+
+public class movement : MonoBehaviour
+{
+    Rigidbody rb;
+    Animator anim;
+    Vector3 destination;
+    public float speed = 4.0f;
+
+    void Start () {
+        anim = GetComponent <Animator> ();
+        rb = GetComponent<Rigidbody>();
+    }
+
+  void FixedUpdate()
+    {
+        Vector3 movement_vector = new Vector3(Input.GetAxisRaw("Horizontal"), 0f, Input.GetAxisRaw("Vertical"));
+        rb.MovePosition (rb.position + movement_vector * Time.deltaTime);
+    }
+
+    void Update(){
+        if(Input.GetAxisRaw("Horizontal") < -.01){
+            anim.Play("sideview");
+        }
+        if(Input.GetAxisRaw("Horizontal") > .01){
+            anim.Play("sideview_mirror");
+        }
+        if(Input.GetAxisRaw("Horizontal") > -.01 && Input.GetAxisRaw("Horizontal") < .01){
+            anim.Play("Idle");
+        }
+    }
+}
---
+++ RRPG 100/UnityTutorials-master/Assets/SquishEffect/Scripts/SpriteSquish.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpriteSquish : MonoBehaviour {
+
+	private float _counter = 0f;
+
+	[SerializeField] private Vector2 _from,_to;
+	[SerializeField] private float _time = 8f;
+
+	public void OnTriggerEnter2D(Collider2D other) {
+		InvokeRepeating ("LerpLocalScale", 0, Time.deltaTime);
+	}		
+		
+	void LerpLocalScale() {
+
+		if (_counter < 1) {
+
+			_counter += Time.deltaTime * _time;
+
+			transform.localScale = Vector2.Lerp (_from, _to, _counter);
+
+		} else {
+			_counter = 0f;
+			transform.localScale = new Vector2 (1, 1);
+			CancelInvoke ("LerpLocalScale");
+		}
+
+	}
+}
