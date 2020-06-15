de61e5d816f63a735ed9416396051c83b4953a27
benjiferoce <31674458+benjiferoce@users.noreply.github.com>
2020-06-15 16:35:31 -0400

    commit

--- RRPG100_Demo/Assets/Scripts/Unit.cs
+++ RRPG100_Demo/Assets/Scripts/Unit.cs
@@ -5,7 +5,9 @@ using UnityEngine;
 public class Unit : MonoBehaviour
 {
     public string unitName;
-    public int damage;
+    public int attack;
+    public int defense;
+    public int multiplier;
     public int maxHP;
     public int currentHP;
     public int speed;
